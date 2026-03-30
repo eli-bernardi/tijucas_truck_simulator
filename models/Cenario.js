2cefe727b910474991ace192fdf2db6ff505e172
Eliel Bernardi <elielbernardi0012@gmail.com>
2026-03-30 08:14:05 -0300

    Retirando Arquivos musica.js dos htm

--- models/Cenario.js
+++ models/Cenario.js
@@ -4,7 +4,6 @@ class Cenario {
         this.offset = 0
         this.vel = 3
 
-        // Uma imagem por fase
         this.fundos = {
             1: this._carregarImg('../assets/images/game/fase1.png'),
             2: this._carregarImg('../assets/images/game/fase2.png'),
@@ -20,40 +19,38 @@ class Cenario {
 
     _desenharCamada(img, offset, y, h) {
         if (!img.complete || img.naturalWidth === 0) return
-        const w = 1600  // força a largura independente do tamanho real da imagem
+        const w = 1600
         const ox = ((offset % w) + w) % w
         des.drawImage(img, -ox, y, w, h)
         des.drawImage(img, -ox + w, y, w, h)
     }
 
     desenhar() {
-        // Fundo único rolando
-        this._desenharCamada(this.fundos[this.fase], this.offset, 0, 500) // altura 500
-
+        this._desenharCamada(this.fundos[this.fase], this.offset, 0, 500)
 
         // Linha de entrega
         des.strokeStyle = '#f1c40f'
         des.lineWidth = 5
         des.setLineDash([10, 6])
         des.beginPath()
-        des.moveTo(1500, 0)   // linha de entrega perto da borda direita
+        des.moveTo(1500, 0)
         des.lineTo(1500, 500)
-
         des.stroke()
         des.setLineDash([])
 
         // Placa de entrega
         des.fillStyle = 'rgba(0,0,0,0.55)'
-        des.fillRect(15000, 355, 84, 28)
+        des.fillRect(1450, 10, 100, 28)   // ← corrigido
         des.fillStyle = '#f1c40f'
         des.font = 'bold 13px Arial'
         des.textAlign = 'center'
-        des.fillText('🚩 ENTREGA', 1500, 300)
+        des.fillText('🚩 ENTREGA', 1500, 29)  // ← corrigido
     }
 
     atualizarLinhas() {
         this.offset += this.vel
     }
+
     resetar() {
         this.fase = 1
         this.offset = 0
